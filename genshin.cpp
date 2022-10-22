8122bdc9dbdfe5a48cef79c01e83bf9f0ac3ac95
Arihant Garg <arihantgarg.ishu@gmail.com>
2022-10-22 19:11:31 +0530

    Xiangling added

--- genshin.cpp
+++ genshin.cpp
@@ -92,6 +92,17 @@ double swirl(Character c)
     return outgoingdmg;
 }
 
+double overload(Character c)
+{
+    //Assumes no overload buff
+    if(c.resMult>1)
+        c.resMult=1+(c.resMult-1)/2;
+
+    double incomingdmg = 2 * c.lvlMult * ( 1 + ((16*c.em)/(c.em+2000)) );
+    double outgoingdmg = incomingdmg * c.resMult;
+    return outgoingdmg;
+}
+
 double dmg(Character c,double basedmg,double dmg,double resMult)
 {
     if(c.cr>100)
@@ -157,7 +168,7 @@ void venti()
         venti.skilldmg+=venti.dmg+48+15+50;
         venti.burstdmg+=venti.dmg+48+15+50;
 
-        // Assume aoe
+        // Assume aoe, 18 swirls total
 
         double reactiondmg = 2 * (2+7) * swirl(venti);
         double skilldmg = 2 * edmg ( venti , venti.atk*venti.skilltalent/100 );
@@ -166,7 +177,7 @@ void venti()
 
         double totaldmg=reactiondmg+skilldmg+burstdmg+infusiondmg;
 
-        cout<<"\n DAMAGE = "<<(double)totaldmg<<"\n\n";
+        cout<<"\n DAMAGE = "<<totaldmg<<"\n\n";
     }
     else
     {
@@ -177,11 +188,84 @@ void venti()
 
 
 
+//Xiangling
+void xiangling()
+{
+    Character xiangling;
+    
+    GetStats(&xiangling,1,1,0,0,1);
+
+    // Character Stats( lvl 90 , skill 11 , burst 11 , Weapon=Catch(510))
+
+    xiangling.baseatk=225+510;
+    xiangling.basedef=669;
+    xiangling.basehp=10875;
+    xiangling.skilltalent=211.43;
+    xiangling.bursttalent=200; //averaged approx
+
+
+    // TEAM
+
+    cout<<"Enter team name : ";
+    string comp;
+    cin>>comp;
+    if(comp=="rational")
+    {
+        //RATIONAL ( Weapon=Catch , Artifact = 4Emblem , Resonance=2 PYRO(25% atk) ; Buffs= 23.2%burstdmg( raiden ) ,-921atk( bennett ) , -5% PyroRes( xiangling ) )
+
+        xiangling.cr+=11.5; //averaged approx
+        xiangling.skilldmg+=xiangling.dmg;
+        xiangling.burstdmg+=xiangling.dmg+32+0.2*xiangling.er+23.2;
+        xiangling.atk+=xiangling.baseatk*0.25; //Assumes stats were taken when xiangling stats dont include buffs
+        xiangling.resMult+=0.08; //averaged approx
+
+        // Assume single target, 14 overloads, 90% burst vaporise , 50% skill vaporise
+
+        double reactiondmg = 16 * overload(xiangling);
+        double vaporiseMult= 1.5 * (1 + (2.78 * xiangling.em/(xiangling.em+1400)));
+        double skilldmg = 4 * edmg ( xiangling , xiangling.atk*xiangling.skilltalent/100 ) * ( 0.5 * vaporiseMult + 0.5 );
+        double burstdmg = 14 * qdmg ( xiangling ,xiangling.atk*xiangling.bursttalent/100 ) * ( 0.9 * vaporiseMult + 0.1 );
+        double totaldmg=reactiondmg+skilldmg+burstdmg;
+
+        cout<<"\n DAMAGE = "<<totaldmg<<"\n\n";
+    }
+    else if(comp=="yelanrational")
+    {
+        // C0 Yelan assumed
+        //YELANRATIONAL ( Weapon=Catch , Artifact = 4Emblem , Resonance=2 PYRO(25% atk) ; Buffs= , 20%atk( yelan-elegy ) , 100em( yelan-elegy) , 23.2%burstdmg( raiden ) ,-921atk( bennett ) , -5% PyroRes( xiangling ) )
+
+        xiangling.cr+=11.5; //averaged approx
+        xiangling.skilldmg+=xiangling.dmg;
+        xiangling.burstdmg+=xiangling.dmg+32+0.2*xiangling.er+23.2;
+        xiangling.atk+=xiangling.baseatk*(0.2+0.25); //Assumes stats were taken when xiangling stats dont include buffs
+        xiangling.resMult+=0.08; //averaged approx
+        xiangling.em+=100;
+
+        // Assume single target, 14 overloads, 75% burst vaporise , 40% skill vaporise
+
+        double reactiondmg = 16 * overload(xiangling);
+        double vaporiseMult= 1.5 * (1 + (2.78 * xiangling.em/(xiangling.em+1400)));
+        double skilldmg = 4 * edmg ( xiangling , xiangling.atk*xiangling.skilltalent/100 ) * ( 0.4 * vaporiseMult + 0.6 );
+        double burstdmg = 14 * qdmg ( xiangling ,xiangling.atk*xiangling.bursttalent/100 ) * ( 0.75 * vaporiseMult + 0.25 );
+        double totaldmg=reactiondmg+skilldmg+burstdmg;
+
+        cout<<"\n DAMAGE = "<<totaldmg<<"\n\n";
+    }
+    else
+    {
+        cout<<"No such team exists";
+    }
+}
+
+
+
 
 void CheckCharacter(string s)
 {
     if(s=="venti")
         venti();
+    else if(s=="xiangling")
+        xiangling();
     else
         cout<<"This character has no TC yet";
 }
